214af8a532e851f25963794523b3b096a824790d
SadSack963 <j_patmore@yahoo.co.uk>
2020-11-17 22:12:59 +0000

    Bare bones done

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ if cards["Player"]["score"] == 21:
         print("You win! You have Blackjack.")
     print(f'Your hand:   {cards["Player"]["hand"]}, score: {cards["Player"]["score"]}')
     print(f'Dealer hand: {cards["Dealer"]["hand"]}, score: {cards["Dealer"]["score"]}')
-    play = 0
+    play = False
 
 # Another card for Player
 new_card = input("Do you want another card? Y/(N) ").lower()
@@ -48,4 +48,50 @@ if new_card == "y":
     cards["Player"]["score"] += card
     print(f'Your hand:   {cards["Player"]["hand"]}, score: {cards["Player"]["score"]}')
 
+    # Check Player score.
+    # If > 21 then check for Ace and make value = 1
+    if cards["Player"]["score"] > 21: 
+        bust = True
+        while bust:
+            for i in cards["Player"]["hand"]:
+                if cards["Player"]["hand"][i] == 11:
+                    cards["Player"]["hand"][i] == 1
+                    cards["Player"]["score"] -= 10
+                    bust = False
+    if bust:
+        print(f'You Lose! You bust with a score of {cards["Player"]["score"]}')
+    else:
+        # TODO Pick another card?
+        print()
+
+# New Dealer cards
+while cards["Dealer"]["score"] < 16:
+    card = random.choice(deck)
+    cards["Dealer"]["hand"].append(card)
+    cards["Dealer"]["score"] += card
+    print(f'Dealer\'s hand:   {cards["Dealer"]["hand"]}, score: {cards["Dealer"]["score"]}')
+
+    # Check Dealer score.
+    # If > 21 then check for Ace and make value = 1
+    if cards["Dealer"]["score"] > 21: 
+        bust = True
+        while bust:
+            for i in cards["Dealer"]["hand"]:
+                if cards["Dealer"]["hand"][i] == 11:
+                    cards["Dealer"]["hand"][i] == 1
+                    cards["Dealer"]["score"] -= 10
+                    bust = False
+    if bust:
+        print(f'You Win! Dealer bust with a score of {cards["Dealer"]["score"]}')
+
+# Compare scores to decide winner
+if cards["Dealer"]["score"] > cards["Player"]["score"]:
+    print("You Lose!")
+if cards["Dealer"]["score"] == cards["Player"]["score"]:
+    print("Draw")
+if cards["Dealer"]["score"] > cards["Player"]["score"]:
+    print("You Win!")
+print(f'Your hand:   {cards["Player"]["hand"]}, score: {cards["Player"]["score"]}')
+print(f'Dealer hand: {cards["Dealer"]["hand"]}, score: {cards["Dealer"]["score"]}')
+play = False
 
